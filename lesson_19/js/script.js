de3cb0f1a84793391edf75dc25dbfdca94d530d2
Sergey <61992933+f4stt@users.noreply.github.com>
2025-12-11 08:24:19 +0200

    added lesson_19

---
+++ lesson_19/js/script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+document.addEventListener('click', documentActions)
+
+function documentActions(e) {
+	const targetElement = e.target
+
+	if (targetElement.closest('.icon-menu')) {
+		document.documentElement.toggleAttribute('data-menu-open')
+	}
+
+}
+
+
